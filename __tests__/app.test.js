c457713fa6414f658a79d508fa359e61c11b622f
Piotr-Szyszkowski <peter.szyszkowski@protonmail.com>
2022-12-20 11:49:39 +0000

    broke api-controller.js on purpose for testing, started on app.test.js

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,1 +1,8 @@
-console.log("Test file firing up!");
+console.log("*** app.test.js file firing up! ***");
+
+const db = require(`../db/connection`);
+const app = require("../app.js");
+const request = require("supertest");
+const testData = require("../db/data/test-data/index");
+const seed = require("../db/seeds/seed");
+const endpoints = require(`../all-endpoints`);
--- controllers/api-controller.js
+++ controllers/api-controller.js
@@ -1,7 +1,8 @@
 const endpoints = require("../all-endpoints");
 
 const sendHelloFromApi = (request, response, next) => {
-  response.status(200).send({ endpoints });
+  // response.status(200).send({ endpoints });
+  response.status(200).send("random something");
 };
 
 module.exports = sendHelloFromApi;
